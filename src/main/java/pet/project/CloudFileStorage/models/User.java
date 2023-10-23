371618fcd33f5d43e3e572fe38313e9f1d126abe
urgmaker <uranovrg@gmail.com>
2023-10-23 16:38:39 +0300

    Add User

---
+++ src/main/java/pet/project/CloudFileStorage/models/User.java
@@ -0,0 +1,38 @@
+package pet.project.CloudFileStorage.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", schema = "cloud_file_storage")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @UuidGenerator
+    private UUID id;
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private String username;
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private String email;
+    private Set<Role> roles;
+}
