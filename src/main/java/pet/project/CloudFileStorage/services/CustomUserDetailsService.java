c53a4373af882a0ed4de40ae706b72e121390c46
urgmaker <uranovrg@gmail.com>
2023-10-23 17:11:30 +0300

    Add CustomUserDetailsService

---
+++ src/main/java/pet/project/CloudFileStorage/services/CustomUserDetailsService.java
@@ -0,0 +1,55 @@
+package pet.project.CloudFileStorage.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pet.project.CloudFileStorage.models.Role;
+import pet.project.CloudFileStorage.models.User;
+import pet.project.CloudFileStorage.repositories.UserRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
+
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                enabled,
+                accountNonExpired,
+                credentialsNonExpired,
+                accountNonLocked,
+                getAuthorities(user.getRoles())
+        );
+    }
+
+    private static Set<SimpleGrantedAuthority> getAuthorities(Set<Role> roles) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.name()));
+        }
+        return authorities;
+    }
+}
