b235dfdd0f8a3d29d494083b5dfa69759112bbcf
r.uranov <uranovrg@gmail.com>
2023-10-24 12:46:30 +0300

    Add LoginController

---
+++ src/main/java/pet/project/CloudFileStorage/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package pet.project.CloudFileStorage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping
+    public String showLoginPage() {
+        return "login";
+    }
+}
