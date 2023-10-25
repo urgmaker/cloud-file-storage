e9ffa2b637262f14d1be10212cf02ff9ff1270bf
r.uranov <uranovrg@gmail.com>
2023-10-25 11:36:17 +0300

    Change CustomUserDetailsService.getAuthorities

--- src/main/java/pet/project/CloudFileStorage/services/CustomUserDetailsService.java
+++ src/main/java/pet/project/CloudFileStorage/services/CustomUserDetailsService.java
@@ -7,13 +7,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pet.project.CloudFileStorage.models.Role;
 import pet.project.CloudFileStorage.models.User;
 import pet.project.CloudFileStorage.repositories.UserRepository;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
