61ef879d642c71eef461154837bdee29c94c4e8f
r.uranov <uranovrg@gmail.com>
2023-10-26 13:25:44 +0300

    Add FileOperationException

---
+++ src/main/java/pet/project/CloudFileStorage/exceptions/file/FileOperationException.java
@@ -0,0 +1,11 @@
+package pet.project.CloudFileStorage.exceptions.file;
+
+public class FileOperationException extends RuntimeException {
+    public FileOperationException(String message) {
+        super(message);
+    }
+
+    public FileOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
