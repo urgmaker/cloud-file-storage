faa4dccf718002abf40877ce2a3651cc98b84220
urgmaker <uranovrg@gmail.com>
2023-10-28 14:19:04 +0300

    Add FileService.uploadFile

--- src/main/java/pet/project/CloudFileStorage/services/FileService.java
+++ src/main/java/pet/project/CloudFileStorage/services/FileService.java
@@ -3,12 +3,17 @@ package pet.project.CloudFileStorage.services;
 import io.minio.GetObjectArgs;
 import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import pet.project.CloudFileStorage.config.minio.MinioBucketConfiguration;
 import pet.project.CloudFileStorage.dto.file.FileDownloadDto;
+import pet.project.CloudFileStorage.dto.file.FileUploadDto;
 import pet.project.CloudFileStorage.exceptions.file.FileOperationException;
+
+import java.io.InputStream;
 
 import static pet.project.CloudFileStorage.utils.MinioRootFolder.getUserRootFolderPrefix;
 
@@ -32,6 +37,19 @@ public class FileService {
             return new ByteArrayResource(object.readAllBytes());
         } catch (Exception e) {
             throw new FileOperationException("Download error! Please, try again later.");
+        }
+    }
+
+    public void uploadFile(FileUploadDto fileUploadDto) {
+        MultipartFile file = fileUploadDto.getFile();
+        try (InputStream inputStream = file.getInputStream()) {
+            minioClient.putObject(PutObjectArgs.builder()
+                    .stream(inputStream, file.getSize(), -1)
+                    .bucket(minioBucketConfiguration.getBucketName())
+                    .object(getUserRootFolderPrefix(fileUploadDto.getOwner()) + file.getOriginalFilename())
+                    .build());
+        } catch (Exception e) {
+            throw new FileOperationException("There is an error while uploading the file, try again later");
         }
     }
 }
