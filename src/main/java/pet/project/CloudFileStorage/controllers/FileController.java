3e549a891113657ffbdea38404809d21733b806d
urgmaker <uranovrg@gmail.com>
2023-10-28 14:40:41 +0300

    Add FileController.deleteFile

--- src/main/java/pet/project/CloudFileStorage/controllers/FileController.java
+++ src/main/java/pet/project/CloudFileStorage/controllers/FileController.java
@@ -10,9 +10,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import pet.project.CloudFileStorage.dto.file.FileDeleteDto;
 import pet.project.CloudFileStorage.dto.file.FileDownloadDto;
 import pet.project.CloudFileStorage.dto.file.FileRenameDto;
 import pet.project.CloudFileStorage.dto.file.FileUploadDto;
+import pet.project.CloudFileStorage.exceptions.file.InvalidFileDeleteException;
 import pet.project.CloudFileStorage.exceptions.file.InvalidFileDownloadException;
 import pet.project.CloudFileStorage.exceptions.file.InvalidFileRenameException;
 import pet.project.CloudFileStorage.exceptions.file.InvalidFileUploadException;
@@ -32,7 +34,7 @@ public class FileController {
     @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public ResponseEntity<ByteArrayResource> downloadFile(@Valid @ModelAttribute("fileDownloadDto")
-                                                              FileDownloadDto fileDownloadDto,
+                                                          FileDownloadDto fileDownloadDto,
                                                           BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new InvalidFileDownloadException(ResponseError.getErrorMessage(bindingResult));
@@ -66,6 +68,19 @@ public class FileController {
         fileService.renameFile(fileRenameDto);
 
         redirectAttributes.addFlashAttribute("success", "File renamed successfully");
+        return new RedirectView("/");
+    }
+
+    @DeleteMapping
+    public RedirectView deleteFile(@Valid @ModelAttribute("fileDeleteDto") FileDeleteDto fileDeleteDto,
+                                   BindingResult bindingResult,
+                                   RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidFileDeleteException(ResponseError.getErrorMessage(bindingResult));
+        }
+        fileService.deleteFile(fileDeleteDto);
+
+        redirectAttributes.addFlashAttribute("success", "File deleted successfully");
         return new RedirectView("/");
     }
 }
