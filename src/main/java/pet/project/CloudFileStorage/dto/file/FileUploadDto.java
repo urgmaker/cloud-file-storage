4af94be81c0274d39660da487d8a8e69c0fe7ce0
urgmaker <uranovrg@gmail.com>
2023-10-28 14:26:10 +0300

    Update FileUploadDto

--- src/main/java/pet/project/CloudFileStorage/dto/file/FileUploadDto.java
+++ src/main/java/pet/project/CloudFileStorage/dto/file/FileUploadDto.java
@@ -1,6 +1,5 @@
 package pet.project.CloudFileStorage.dto.file;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
