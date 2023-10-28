e587f71d80b65cdd8d1dc615cec8bc0e8b51d54d
urgmaker <uranovrg@gmail.com>
2023-10-28 14:37:20 +0300

    Add FileService.deleteFile

--- src/main/java/pet/project/CloudFileStorage/services/FileService.java
+++ src/main/java/pet/project/CloudFileStorage/services/FileService.java
@@ -1,15 +1,14 @@
 package pet.project.CloudFileStorage.services;
 
-import io.minio.GetObjectArgs;
-import io.minio.GetObjectResponse;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import pet.project.CloudFileStorage.config.minio.MinioBucketConfiguration;
+import pet.project.CloudFileStorage.dto.file.FileDeleteDto;
 import pet.project.CloudFileStorage.dto.file.FileDownloadDto;
+import pet.project.CloudFileStorage.dto.file.FileRenameDto;
 import pet.project.CloudFileStorage.dto.file.FileUploadDto;
 import pet.project.CloudFileStorage.exceptions.file.FileOperationException;
 
@@ -51,5 +50,59 @@ public class FileService {
         } catch (Exception e) {
             throw new FileOperationException("There is an error while uploading the file, try again later");
         }
+    }
+
+    public void renameFile(FileRenameDto fileRenameDto) {
+        try {
+            minioClient.copyObject(CopyObjectArgs.builder()
+                    .bucket(minioBucketConfiguration.getBucketName())
+                    .object(getPathWithNewName(
+                            getUserRootFolderPrefix(fileRenameDto.getOwner() + fileRenameDto.getPath()),
+                            fileRenameDto.getCurrentName(),
+                            fileRenameDto.getNewName()
+                    ))
+                    .source(CopySource.builder()
+                            .bucket(minioBucketConfiguration.getBucketName())
+                            .object(getUserRootFolderPrefix(fileRenameDto.getOwner() + fileRenameDto.getPath()))
+                            .build())
+                    .build());
+
+            FileDeleteDto fileDeleteDto = new FileDeleteDto();
+
+            fileDeleteDto.setPath(fileRenameDto.getPath());
+            fileDeleteDto.setOwner(fileRenameDto.getOwner());
+
+            deleteFile(fileDeleteDto);
+        } catch (Exception e) {
+            throw new FileOperationException("There is an error while renaming the file, try again later");
+        }
+    }
+
+    public void deleteFile(FileDeleteDto fileDeleteDto) {
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(minioBucketConfiguration.getBucketName())
+                    .object(getUserRootFolderPrefix(fileDeleteDto.getOwner()) + fileDeleteDto.getPath())
+                    .build());
+        } catch (Exception e) {
+            throw new FileOperationException("There is an error while deleting the file, try again later");
+        }
+    }
+
+    private static String getFileNameFromPath(String path) {
+        if (!path.contains("/")) {
+            return path;
+        }
+        if (path.endsWith("/")) {
+            path = path.substring(0, path.length() - 1);
+        }
+        if (path.lastIndexOf('/') > 0) {
+            return path.substring(path.lastIndexOf('/') + 1);
+        }
+        return path;
+    }
+
+    private static String getPathWithNewName(String path, String oldName, String newName) {
+        return path.replace(oldName, newName);
     }
 }
