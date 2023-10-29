701a91c308028f7c182327aa2ea21b9dac39b5c7
urgmaker <uranovrg@gmail.com>
2023-10-29 14:19:01 +0300

    Add FolderController

---
+++ src/main/java/pet/project/CloudFileStorage/controllers/FolderController.java
@@ -0,0 +1,51 @@
+package pet.project.CloudFileStorage.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import pet.project.CloudFileStorage.dto.folder.FolderUploadDto;
+import pet.project.CloudFileStorage.exceptions.folder.InvalidFolderUploadException;
+import pet.project.CloudFileStorage.services.FolderService;
+import pet.project.CloudFileStorage.utils.ResponseError;
+
+@Controller
+@RequestMapping("/folders")
+public class FolderController {
+    private final FolderService folderService;
+
+    public FolderController(FolderService folderService) {
+        this.folderService = folderService;
+    }
+
+    @PostMapping
+    public RedirectView uploadFolder(@Valid @ModelAttribute("folderUploadDto") FolderUploadDto folderUploadDto,
+                                     BindingResult bindingResult,
+                                     RedirectAttributes redirectAttributes) {
+
+        if (isFolderEmpty(folderUploadDto)) {
+            throw new InvalidFolderUploadException("Empty folder upload is not supported");
+        }
+        if (bindingResult.hasErrors()) {
+            throw new InvalidFolderUploadException(ResponseError.getErrorMessage(bindingResult));
+        }
+
+        folderService.uploadFolder(folderUploadDto);
+
+        redirectAttributes.addFlashAttribute("success", "Folder uploaded successfully");
+        return new RedirectView("/");
+    }
+
+    private boolean isFolderEmpty(FolderUploadDto folderUploadDto) {
+        if (folderUploadDto.getFiles().size() == 1) {
+            String filename = folderUploadDto.getFiles().get(0).getOriginalFilename();
+
+            return filename == null || filename.isBlank();
+        }
+        return false;
+    }
+}
